7b8fcdfddaa871f8dfd4d72e5a38aa778e7774e2
Hieu Minh Nguyen <1174029+cheehieu@users.noreply.github.com>
2024-05-17 19:06:35 -0500

    Adds Github Actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const apiKey = process.env.ELEVEN_LABS_API_KEY;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <code>hello world</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <code>key: {apiKey}</code>
+        </p>
       </header>
     </div>
   );
