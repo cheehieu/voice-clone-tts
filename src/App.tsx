02e481b036a8b4162eb636fb599ec84917737c01
Hieu Minh Nguyen <1174029+cheehieu@users.noreply.github.com>
2024-05-17 23:20:10 -0500

    Add MUI placeholder

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,59 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import TextField from "@mui/material/TextField";
 import "./App.css";
 
 const apiKey = process.env.ELEVEN_LABS_API_KEY ?? "no key found";
 
 function App() {
+  const [textInput, setTextInput] = useState<string>("");
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>hello world</code>
+          <code>voice-clone-tts</code>
         </p>
-        <p>
-          <code>🔑: {apiKey}</code>
-        </p>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 5, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              sx={{
+                "& .MuiInputBase-root": {
+                  color: "white",
+                },
+              }}
+              color="info"
+              focused
+              label="Text Input"
+              multiline
+              placeholder="Enter text to convert to speech"
+              rows={8}
+              variant="outlined"
+              value={textInput}
+              onChange={(event) => {
+                setTextInput(event.target.value);
+              }}
+            />
+          </div>
+        </Box>
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          onClick={() => {
+            alert(`Text Input: ${textInput}`);
+          }}
+        >
+          Submit
+        </Button>
       </header>
     </div>
   );
